335897930f73cfb6878f17dea66ab52dbe01b889
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-08-01 21:02:16 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -5,11 +5,11 @@ from send_mail import send_mail
 
 main = Blueprint('main', __name__)
 
-main.route('/')
+@main.route('/')
 def index():
     return render_template('index.html')
 
-main.route('/success',methods=['GET', 'POST'] )
+@main.route('/success',methods=['GET', 'POST'] )
 def success():
     if request.method == "POST":
         name = request.form['name']
@@ -18,30 +18,26 @@ def success():
         send_mail(name=name, email=email, message=message)
     return render_template('success.html', name=name, email=email)
 
-main.route('/project01')
-def project01():
-    return render_template('project01.html')
-
-main.route('/project02')
+@main.route('/project02')
 def project02():
     return render_template('project02.html')
 
-main.route('/project03')
+@main.route('/project03')
 def project03():
     return render_template('project03.html')
 
-main.route('/project04')
+@main.route('/project04')
 def project04():
     return render_template('project04.html')
 
-main.route('/project05')
+@main.route('/project05')
 def project05():
     return render_template('project05.html')
 
-main.app_errorhandler(404)
+@main.app_errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-main.app_errorhandler(500)
+@main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
